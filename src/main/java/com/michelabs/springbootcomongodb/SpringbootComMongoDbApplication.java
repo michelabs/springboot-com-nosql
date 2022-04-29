b9d74763a1c38069c32fa84bf07b91e1e7a0ef4b
Michel Silva <michel.barbosa@hotmail.com>
2022-04-29 10:47:15 -0300

    project created

---
+++ src/main/java/com/michelabs/springbootcomongodb/SpringbootComMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.michelabs.springbootcomongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootComMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootComMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelabs/springbootcomongodb/SpringbootComMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelabs.springbootcomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootComMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
