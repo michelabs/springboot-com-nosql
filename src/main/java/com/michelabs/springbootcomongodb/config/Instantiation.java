7c9e54386986f33e062f0eef2a0721518477ab2a
Michel Silva <michel.barbosa@hotmail.com>
2022-04-29 13:23:22 -0300

    returning informations with DTO and creating data base instantiation

---
+++ src/main/java/com/michelabs/springbootcomongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.michelabs.springbootcomongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.michelabs.springbootcomongodb.entities.User;
+import com.michelabs.springbootcomongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User user1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User user2 = new User(null, "Alex Green", "alex@gmail.com");
+		User user3 = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(user1, user2, user3));		
+		
+	}
+
+}
--- src/main/java/com/michelabs/springbootcomongodb/controllers/UserController.java
+++ src/main/java/com/michelabs/springbootcomongodb/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.michelabs.springbootcomongodb.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.michelabs.springbootcomongodb.dto.UserDTO;
 import com.michelabs.springbootcomongodb.entities.User;
 import com.michelabs.springbootcomongodb.services.UserService;
 
@@ -20,9 +21,10 @@ public class UserController {
 	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAllUsers() {		
+	public ResponseEntity<List<UserDTO>> findAllUsers() {		
 		List<User> listUsers = userService.findAllUsers();
-		return ResponseEntity.ok().body(listUsers);
+		List<UserDTO> listDto = listUsers.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}	
 	
 }
