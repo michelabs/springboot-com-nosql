047b24a1df150b3934ecb7ef9ea56eafcfb71512
Michel Silva <michel.barbosa@hotmail.com>
2022-04-29 15:02:40 -0300

    implementing Post method

--- src/main/java/com/michelabs/springbootcomongodb/controllers/UserController.java
+++ src/main/java/com/michelabs/springbootcomongodb/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.michelabs.springbootcomongodb.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.michelabs.springbootcomongodb.dto.UserDTO;
 import com.michelabs.springbootcomongodb.entities.User;
@@ -34,4 +38,11 @@ public class UserController {
 		return ResponseEntity.ok().body(new UserDTO(user)); 		
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody UserDTO userDTO) {
+		User user = userService.userFromDTO(userDTO);
+		user = userService.insertUser(user);
+		URI userCreated = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(userCreated).body(user);
+	}	
 }
--- src/main/java/com/michelabs/springbootcomongodb/services/UserService.java
+++ src/main/java/com/michelabs/springbootcomongodb/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.michelabs.springbootcomongodb.dto.UserDTO;
 import com.michelabs.springbootcomongodb.entities.User;
 import com.michelabs.springbootcomongodb.repository.UserRepository;
 import com.michelabs.springbootcomongodb.services.exception.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findUserById(String id) {
 		Optional<User> user = userRepository.findById(id);		
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insertUser(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User userFromDTO (UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());		
 	}
 }
