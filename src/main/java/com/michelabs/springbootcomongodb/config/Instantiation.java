c5e926a97f0402a3cb2e3ac823383392aca96d7c
Michel Silva <michel.barbosa@hotmail.com>
2022-05-01 11:00:59 -0300

    referencing posts with users

--- src/main/java/com/michelabs/springbootcomongodb/config/Instantiation.java
+++ src/main/java/com/michelabs/springbootcomongodb/config/Instantiation.java
@@ -42,7 +42,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP. Abraçoes!", new AuthorDTO(user1));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", new AuthorDTO(user1));		
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));		
+		postRepository.saveAll(Arrays.asList(post1, post2));	
+		
+		user1.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(user1);
 		
 	}
 }
