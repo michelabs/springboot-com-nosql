6acda20008f41a47d1bff10a27dd85a1909f5dc1
Michel Silva <michel.barbosa@hotmail.com>
2022-05-07 16:24:50 -0300

    adding comments in posts

--- src/main/java/com/michelabs/springbootcomongodb/config/Instantiation.java
+++ src/main/java/com/michelabs/springbootcomongodb/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.michelabs.springbootcomongodb.dto.AuthorDTO;
+import com.michelabs.springbootcomongodb.dto.CommentDTO;
 import com.michelabs.springbootcomongodb.entities.Post;
 import com.michelabs.springbootcomongodb.entities.User;
 import com.michelabs.springbootcomongodb.repository.PostRepository;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP. Abraçoes!", new AuthorDTO(user1));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", new AuthorDTO(user1));		
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(user2));
+		CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(user3));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(user2));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));	
 		
